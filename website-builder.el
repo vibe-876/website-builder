0b988941d67bb2d5d6806d1f9a47d81248cbaa53
cam <cameronayres123@gmail.com>
2026-05-03 06:32:56 +0100

    Added support for RSS.

--- website-builder.el
+++ website-builder.el
@@ -2,14 +2,18 @@
 
 
 (setq top-html "<!DOCTYPE html><head><title>Blog</title><link rel=\"stylesheet\" href=\"styles.css\"><body><h1>Blog Entries</h1><a href=\"index.html\">Back Home</a><p>Please note that these are not in the order they were posted -- I'm currently working on that.<ul>"
-      bottom-html "</ul></body>")
+      bottom-html "</ul></body>"
+      top-rss "<?xml version=\"1.0\" encoding=\"UTF-8\" ?><rss version=\"2.0\"><channel>"
+      link-rss "https://vibe-876.github.io"
+      bottom-rss "</channel></rss>")
 
 
 (defun cam/build-website ()
   (interactive)
   (cam/build-website-directory "." "styles.css")
   (cam/build-website-directory "posts" "../styles.css")
-  (cam/build-website-posts-page "posts"))
+  (cam/build-website-posts-page "posts")
+  (cam/generate-rss "posts"))
 
 (defun cam/build-website-posts-page (post-directory)
   "Constructs a HTML page that lists all blog entries, in the order
@@ -56,5 +60,31 @@ pandoc."
 			 (file-name-with-extension file-name ".html")
 			 " --css=\"" css-file "\" -s "
 			 "--syntax-highlighting=\"espresso\"")))
+
+(defun cam/generate-rss (post-directory)
+  "Generates an RSS feed, based on the Org files in POSTS-DIRECTORY.
+This will use the title and date set in the Org file's tags."
+  (let ((org-files (directory-files post-directory t "\\.org$"))
+	(old-buffer (current-buffer))
+	(rss-file-name "rss.xml"))
+    
+    (set-buffer (get-buffer-create rss-file-name))
+    (delete-region (point-min) (point-max))
+    (insert top-rss)
+    (mapcar (lambda (file-name)
+	      (insert (concat "<item><title>"
+			      (cam/remove-org-plist-data-from-text 
+			       (org-publish-find-property file-name :title nil))
+			      "</title><link>" link-rss "/" post-directory "/"
+			      (file-name-nondirectory
+			       (file-name-with-extension file-name ".html"))
+			      "</link><description>>:3</description><content>"
+			      (shell-command-to-string (concat "pandoc -t html " file-name))
+			      "</content></item>")))
+	    org-files)
+    (insert bottom-rss)
+    (write-file rss-file-name)
+    (set-buffer old-buffer)))
+
 
 (provide 'website-builder)
